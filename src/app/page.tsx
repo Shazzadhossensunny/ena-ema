11dfe359705c23f95d1775c00fbcfff631056751
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2025-09-11 16:22:48 +0600

    trust section create

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/home/Hero";
+import Trust from "@/components/home/Trust";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <Trust />
     </>
   );
 }
