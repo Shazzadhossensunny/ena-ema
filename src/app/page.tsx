f740be20a62aae0842bbc952dcf2189df785cdc9
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2025-09-12 22:59:55 +0600

    slider add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,16 @@ import TestimonialsSection from "@/components/home/Testmonial";
 import Trust from "@/components/home/Trust";
 
 const images = [
-  "https://images.unsplash.com/photo-1600585154340-be6161a56a0c",
-  "https://images.unsplash.com/photo-1600585153938-9c99b5d2ff04",
-  "https://images.unsplash.com/photo-1600585153825-54f4d5e1c2e5",
-  "https://images.unsplash.com/photo-1600585153928-96a3f18e51e3",
-  "https://images.unsplash.com/photo-1600585154440-8b9d5e5e5f6d",
+  "/images/sample1.png",
+  "/images/sample2.png",
+  "/images/sample3.png",
+  "/images/sample4.png",
+  "/images/sample5.png",
+  "/images/sample6.png",
+  "/images/sample7.png",
+  "/images/sample8.png",
+  "/images/sample9.png",
+  "/images/sample10.png",
 ];
 
 export default function Home() {
